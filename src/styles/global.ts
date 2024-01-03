564bc4aabc37d126a2c0f2941e417102cfa260ff
Kaíque Zimerer <kaiquezimerer@outlook.com>
2024-01-03 12:29:01 -0300

    Set default theme and global style (styled-componets)

---
+++ src/styles/global.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+    background-color: ${props => props.theme['gray-900']};
+  }
+
+  body, input, textarea, button {
+    font-family: 'Nunito', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 160%;
+    color: ${props => props.theme['gray-200']};
+  }
+`
